eb555b174607ab4d4238ce4153ca0ad1d9bb8c82
Sahil Shaikh <128406259+sahilshaikh867@users.noreply.github.com>
2025-12-04 22:21:21 +0530

    Update app.js

--- backend/ src/app.js
+++ backend/ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import studentRoutes from "./routes/student.routes.js";
 
 const app = express();
 
 app.use(cors());
+app.use("/api/students", studentRoutes);
 app.use(express.json());
 
 // Health check endpoint

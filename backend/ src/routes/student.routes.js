f347f870cda8bb43c3c76eef7283f3d2a68d3e24
Sahil Shaikh <128406259+sahilshaikh867@users.noreply.github.com>
2025-12-04 22:18:42 +0530

    Update student.routes.js

--- backend/ src/routes/student.routes.js
+++ backend/ src/routes/student.routes.js
@@ -1,1 +1,13 @@
+import { Router } from "express";
+import StudentController from "../controllers/student.controller.js";
+
+const router = Router();
+
+router.get("/", StudentController.getAll);
+router.get("/:id", StudentController.getOne);
+router.post("/", StudentController.create);
+router.put("/:id", StudentController.update);
+router.delete("/:id", StudentController.delete);
+
+export default router;
 

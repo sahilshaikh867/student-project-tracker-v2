f6a94c2b50f3badb102ebe68f922b5e932a35886
Sahil Shaikh <128406259+sahilshaikh867@users.noreply.github.com>
2025-12-04 22:33:20 +0530

    Update activity.service.js

--- backend/ src/services/activity.service.js
+++ backend/ src/services/activity.service.js
@@ -1,1 +1,31 @@
+import prisma from "../config/prisma.js";
+
+class ActivityService {
+  
+  // Get all logs for a project
+  static async getLogsByProject(project_id) {
+    return prisma.activityLog.findMany({
+      where: { project_id: Number(project_id) },
+      orderBy: {
+        timestamp: "desc"
+      }
+    });
+  }
+
+  // Create new log entry
+  static async createLog(data) {
+    return prisma.activityLog.create({
+      data
+    });
+  }
+
+  // Delete a log (optional)
+  static async deleteLog(id) {
+    return prisma.activityLog.delete({
+      where: { log_id: Number(id) }
+    });
+  }
+}
+
+export default ActivityService;
 

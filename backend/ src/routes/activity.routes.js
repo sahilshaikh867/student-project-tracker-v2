a643e2b3e215a87b4f30437507b817c9534e8764
Sahil Shaikh <128406259+sahilshaikh867@users.noreply.github.com>
2025-12-04 22:33:59 +0530

    Update activity.routes.js

--- backend/ src/routes/activity.routes.js
+++ backend/ src/routes/activity.routes.js
@@ -1,1 +1,16 @@
+import { Router } from "express";
+import ActivityController from "../controllers/activity.controller.js";
+
+const router = Router();
+
+// Get all logs for a project
+router.get("/project/:project_id", ActivityController.getProjectLogs);
+
+// Create new activity log
+router.post("/", ActivityController.create);
+
+// Delete log
+router.delete("/:id", ActivityController.delete);
+
+export default router;
 

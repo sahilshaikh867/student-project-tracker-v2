d27a58d03323fa7509214fb6953e72e710df91d0
Sahil Shaikh <128406259+sahilshaikh867@users.noreply.github.com>
2025-12-04 22:25:01 +0530

    Update project.routes.js

--- backend/ src/routes/project.routes.js
+++ backend/ src/routes/project.routes.js
@@ -1,1 +1,13 @@
+import { Router } from "express";
+import ProjectController from "../controllers/project.controller.js";
+
+const router = Router();
+
+router.get("/", ProjectController.getAll);
+router.get("/:id", ProjectController.getOne);
+router.post("/", ProjectController.create);
+router.put("/:id", ProjectController.update);
+router.delete("/:id", ProjectController.delete);
+
+export default router;
 

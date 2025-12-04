61f213b52eff25500753075a03ad73b1669796aa
Sahil Shaikh <128406259+sahilshaikh867@users.noreply.github.com>
2025-12-04 22:22:49 +0530

    Update mentor.routes.js

--- backend/ src/routes/mentor.routes.js
+++ backend/ src/routes/mentor.routes.js
@@ -1,1 +1,13 @@
+import { Router } from "express";
+import MentorController from "../controllers/mentor.controller.js";
+
+const router = Router();
+
+router.get("/", MentorController.getAll);
+router.get("/:id", MentorController.getOne);
+router.post("/", MentorController.create);
+router.put("/:id", MentorController.update);
+router.delete("/:id", MentorController.delete);
+
+export default router;
 
